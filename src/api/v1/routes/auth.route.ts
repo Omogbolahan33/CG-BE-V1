247de6120900d876e681df0215cd8f5fc3d7465f
Omogbolahan33 <71589582+Omogbolahan33@users.noreply.github.com>
2025-09-25 15:40:20 +0100

    Update auth.route.ts

--- src/api/v1/routes/auth.route.ts
+++ src/api/v1/routes/auth.route.ts
@@ -1,13 +1,15 @@
 // src/api/v1/routes/auth.route.ts
 
 import { Router } from 'express';
-import * as authController from '../controllers/auth.controller';
+import { login, signup } from '../controllers/auth.controller';
 import { loginRateLimiter } from '../../../middlewares/rateLimiter'; // Import the middleware
 
 const router = Router();
 
 // POST /api/v1/auth/login - Login User (Rate-limited)
 // Apply the rate limiter middleware only to the login route for security
-router.post('/login', loginRateLimiter, authController.login);
-
+router.post('/login', loginRateLimiter, login);
+// Route for new user registration
+// Matches the documentation: /auth/signup
+router.post('/signup', signup); 
 export default router;
