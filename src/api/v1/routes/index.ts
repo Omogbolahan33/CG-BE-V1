2ba6994970f693885a3c917941c4041413293f4c
Omogbolahan33 <71589582+Omogbolahan33@users.noreply.github.com>
2025-09-27 09:08:21 +0100

    Update index.ts

--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -6,6 +6,7 @@ import { Router } from 'express';
 // import userRoutes from './user.route';
 import authRoutes from './auth.route'; 
 import userRouter from './user.routes'; 
+import postRoutes from './post.routes';
 
 const v1Router = Router();
 
@@ -17,5 +18,6 @@ v1Router.use('/auth', authRoutes);
 // Mounts all routes from user.routes.ts under the path /api/v1/users
 // This makes the endpoint: GET /api/v1/users/me
 v1Router.use('/users', userRouter);
+v1router.use('/posts', postRoutes);
 
 export default v1Router;
