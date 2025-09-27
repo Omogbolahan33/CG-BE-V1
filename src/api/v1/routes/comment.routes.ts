5e328d93128d861297cbb1c9866adb7935840d0b
Omogbolahan33 <71589582+Omogbolahan33@users.noreply.github.com>
2025-09-27 18:34:16 +0100

    Update comment.routes.ts

--- src/api/v1/routes/comment.routes.ts
+++ src/api/v1/routes/comment.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { authMiddleware } from '../../../middlewares/auth.middleware';
-import { addCommentController } from '../controllers/comment.controller'; 
+import { addCommentController, editCommentController } from '../controllers/comment.controller'; 
 
 const router = Router({ mergeParams: true });
 
-//ADD COMMENT
+// ADD COMMENT
 // Endpoint: POST /api/v1/posts/:postId/comments
 router.post('/:postId/comments', authMiddleware, addCommentController); 
+
+// EDIT COMMENT
+// Endpoint: PUT /:postId/comments/:commentId
+// This route is relative to the mount point of the comment router in post.routes.ts
+router.put('/:commentId', authMiddleware, editCommentController); 
 
 export default router;
