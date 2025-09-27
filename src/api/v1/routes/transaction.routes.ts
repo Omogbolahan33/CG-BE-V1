484e202b8e9a7c3b606c56f06688d4be0a055dd2
Omogbolahan33 <71589582+Omogbolahan33@users.noreply.github.com>
2025-09-27 19:41:14 +0100

    Update transaction.routes.ts

--- src/api/v1/routes/transaction.routes.ts
+++ src/api/v1/routes/transaction.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { authMiddleware } from '../../../middlewares/auth.middleware';
-import { getTransactionsController } from '../controllers/transaction.controller';
+import { getTransactionsController, createTransactionController } from '../controllers/transaction.controller';
 
 const router = Router();
 
 //  GET USER TRANSACTIONS
 // Endpoint: GET /api/transactions
 router.get('/', authMiddleware, getTransactionsController); 
+
+
+// Endpoint: POST /api/transactions
+router.post('/', authMiddleware, createTransactionController); 
+
 
 export default router;
