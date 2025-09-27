99ecb9b2129481fccf613dc569a8b2f005194297
Omogbolahan33 <71589582+Omogbolahan33@users.noreply.github.com>
2025-09-27 08:33:19 +0100

    Update post.routes.ts

--- src/api/v1/routes/post.routes.ts
+++ src/api/v1/routes/post.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { getPostsController, getPostDetailsController,createPostController  } from '../controllers/post.controller';
-import { authMiddleware } from '../../../middleware/auth.middleware';
+import { authMiddleware } from '../../../middlewares/auth.middleware';
 
 const router = Router();
 
