bbcc875f6f51ed7c639c76b25c7d126babfeee86
Omogbolahan33 <71589582+Omogbolahan33@users.noreply.github.com>
2025-09-27 16:07:50 +0100

    Update post.service.ts

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -478,7 +478,7 @@ export const updatePost = async (
             updateData.price = null;
             updateData.condition = null;
             updateData.brand = null;
-            updateData.deliveryOptions = Prisma.JsonNull.Any;
+            updateData.deliveryOptions = Prisma.JsonNull.DbNull;
             updateData.quantity = null;
         }
     }
