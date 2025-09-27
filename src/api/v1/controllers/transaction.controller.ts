42e26601ee01fa975fbc53ef39138a2a66815834
Omogbolahan33 <71589582+Omogbolahan33@users.noreply.github.com>
2025-09-27 19:28:22 +0100

    Update transaction.controller.ts

--- src/api/v1/controllers/transaction.controller.ts
+++ src/api/v1/controllers/transaction.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { getTransactions } from '../services/transaction.service';
+import { getTransactions } from '../../../services/transaction.service';
 import { UserRole } from '@prisma/client';
 
 // Custom interface for authenticated request
