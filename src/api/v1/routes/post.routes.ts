84269194b1178a8ef815d89123cedbc01f5ae7da
Omogbolahan33 <71589582+Omogbolahan33@users.noreply.github.com>
2025-09-27 07:32:42 +0100

    Update post.routes.ts

--- src/api/v1/routes/post.routes.ts
+++ src/api/v1/routes/post.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import { getPostsController } from '../controllers/post.controller';
+import { getPostsController, getPostDetailsController} from '../controllers/post.controller';
 
 const router = Router();
 
 // Endpoint: GET /api/v1/posts
 router.get('/', getPostsController); 
+
+// Endpoint: GET /api/v1/posts/:postId
+// Fetches a single post's details
+router.get('/:postId', getPostDetailsController);
 
 export default router;
